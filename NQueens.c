8259f98e4f9a200522b6bfc2f227d853cfe4a106
Suraj Jagtap <suraj552003@gmail.com>
2023-02-07 18:08:57 +0530

    done w nQueens

--- NQueens.c
+++ NQueens.c
@@ -1,88 +1,88 @@
+//NQueens Problem
+
 #include<stdio.h>
 #include<stdlib.h>
+#define max 20
 
-int queens(int**, int , int);
-void display(int**, int);
-int isSafe(int**, int, int, int);
+int queens(int[][max], int, int);
+int isSafe(int[][max], int, int, int);
+void display(int[][max], int);
 int min(int, int);
+
 
 int main()
 {
-    int n = 4, i, j;
-    // printf("Enter the No. of Queens: ");
-    // scanf("%d", &n);
-    //create a 2D array of size n*n
-    int** board = (int**)malloc(n * sizeof(int*));
+    int n, i, j, board[max][max];
+    printf("Enter the number of queens: ");
+    scanf("%d", &n);
     for(i = 0; i < n; i++)
     {
-        board[i] = (int*)malloc(n * sizeof(int));
+        for(j = 0; j < n; j++)
+        {
+            board[i][j] = 0;
+        }
     }
-   printf("%d", queens(board, n, 0));
+    printf("Number of Ways: %d\n", queens(board, n, 0));
+    return 0;
 }
 
-int queens(int** board, int n, int row)
+int queens(int board[][max], int n, int row)
 {
-    int col;
+    int col, count = 0;
     if(row == n)
     {
         display(board, n);
         return 1;
     }
 
-    int count = 0;
-
-    //here we are placing the queen and checking for every row and column
-    for(col = 0; col < n; col++)
+    for(col = 0; col < n; col++) 
     {
-        //place the queen if it is safe
-        if (isSafe(board, n, row, col))
+        if(isSafe(board, n, row, col))
         {
             board[row][col] = 1;
             count += queens(board, n, row + 1);
             board[row][col] = 0;
         }
     }
-
     return count;
 }
 
-int isSafe(int** board, int n, int row, int col)
+int isSafe(int board[][max], int n, int row, int col)
 {
-    //check vertical row
     int i;
+    // column
     for(i = 0; i < row; i++)
     {
-        if(board[i][col] == 1)
+        if(board[i][col])
             return 0;
     }
 
-    //diagonal left
+    // diagonal left
     int maxLeft = min(row, col);
-    for(i = 1; i < maxLeft; i++)
+    for(i = 1; i <= maxLeft; i++)
     {
-        if(board[row - i][col - i] == 1)
+        if(board[row - i][col - i])
             return 0;
     }
 
-    //diagonal right
+    // diagonal right
     int maxRight = min(row, n - col - 1);
-    for(i = 1; i < maxRight; i++)
+    for(i = 1; i <= maxRight; i++)  
     {
-        if(board[row - i][col + i] == 1)
+        if(board[row - i][col + i])
             return 0;
     }
-
     return 1;
 }
 
-void display(int** board, int n)
+void display(int board[][max], int n)
 {
     int row, col;
     for(row = 0; row < n; row++)
     {
         for(col = 0; col < n; col++)
         {
-            if(board[row][col] == 1)
+            if(board[row][col])
                 printf("Q ");
             else
                 printf("_ ");
